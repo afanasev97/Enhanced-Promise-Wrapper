8a76461fa3eec21094e00f3bb18f07c93689a806
Ilia Afanasev <afanasev.ilya.a@yandex.ru>
2023-12-26 12:23:09 +0300

    new: modules

--- main.js
+++ main.js
@@ -1,54 +1,23 @@
-const s = new Map();
-let i = 0;
+const EnhancedPromise = require('./enhancedPromise');
+const pendingPromises = require('./pendingPromises');
 
-class P extends Promise {
-	constructor(f) {
-		const wrapF = (resolve, reject) => {
-			const wrapresolve = () => {
-				// console.log("resolve", this.f.toString());
-
-				s.delete(this.n);
-
-				return resolve(...arguments);
-			};
-			const wrapreject = () => {
-				// console.log("reject", this.f.toString());
-
-				s.delete(this.n);
-
-				return reject(...arguments);
-			};
-			
-			return f(wrapresolve, wrapreject);
-		};
-
-		super(wrapF);
-
-		i++;
-
-		this.n = i;
-
-		s.set(this.n, this);
-
-		this.f = f;
-
-		// console.log("start", this.f.toString());
-	}
-}
-
-global.Promise = P;
+// Пример использования
 
 async function test() {
-  const pendingPromise1 = new Promise((resolve) => setTimeout(resolve, 3000));
-  const pendingPromise2 = new Promise((resolve) => setTimeout(resolve, 5000));
-  const pendingPromise3 = new Promise((resolve) =>  { } );
+  const pendingPromise1 = new EnhancedPromise(resolve => setTimeout(resolve, 3000));
+  const pendingPromise2 = new EnhancedPromise(resolve => setTimeout(resolve, 5000));
+  const pendingPromise3 = new EnhancedPromise(() => {});
 
-  console.log('Pending Promises:', Array.from(s.values()).map(p => p.f.toString()));
-
-  // Ждем завершения промисов
+  console.log('Number of Pending Promises Before:', pendingPromises.getCount());
+  console.log('Pending Promises Before:', pendingPromises.getPendingPromises());
+  
   await Promise.all([pendingPromise1, pendingPromise2]);
 
-  console.log('Pending Promises:', Array.from(s.values()).map(p => p.f.toString()));
+  console.log('Number of Pending Promises After:', pendingPromises.getCount());
+  console.log('Pending Promises After:');
+  pendingPromises.getPendingPromises().forEach(({ executor, stack }) => {
+    console.log(executor, stack);
+  });
 }
 
 test();
